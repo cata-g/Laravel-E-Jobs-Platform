8a0d120b2d0b3df24ad32d77e29f1dd652e520b1
imN0oB <49831955+imN0oB@users.noreply.github.com>
2022-09-22 22:25:53 +0300

    laravel + mysql basic setup

---
+++ laragig/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+use Termwind\Components\Raw;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//All Listings
+
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+
+Route::get('/listings/{id}', function($id){
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
+
+
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World<h1>', 200)
+//     ->header('Content-Type', 'text/plain')
+//     ->header('Foo', 'bar');
+// });
+
+// Route::get('/posts/{id}', function($id){
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+
+// Route::get('/search', function (Request $request){
+//     dd($request->name);
+// });
